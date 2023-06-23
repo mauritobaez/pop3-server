f4b1a49d4f61c63b2c437cac7c142429aaa191cb
fshih <fshih@itba.edu.ar>
2023-06-23 12:22:38 -0300

    fix: segv

--- stress-testing/index.js
+++ stress-testing/index.js
@@ -5,7 +5,7 @@ const clients = 900;
 let finished = 0;
 let totaltime = 0;
 
-async function addUsers(users) {
+function addUsers(users) {
     const nc = new NetcatClient();
     return new Promise((resolve) => {
         const client = nc.addr('::1').port(2110).connect().send('a root root\r\n').on('data', (buffer) => {
@@ -14,9 +14,12 @@ async function addUsers(users) {
         for (let i = 0; i < users; i += 1) {
             client.send(`u+ ${i} ${i}\r\n`, () => {
                 total.count += 1;
+                console.log(total.count);
                 if (total.count === users) {
-                    client.close();
-                    resolve();
+                    client.send('q\r\n', () => {
+                        client.close();
+                        resolve();
+                    });
                 }
             })
         }
@@ -36,14 +39,17 @@ async function getEmail(index) {
         host: "localhost",
         port: 1110
     });
-
     const start = process.hrtime();
-    await pop3.RETR(1);
+
+    await pop3.connect();
     finished += 1;
+    await pop3.command('USER', index);
+    await pop3.command('PASS', index);
+    await pop3.command('RETR', 1);
+    await pop3.command('QUIT');
     const seconds = parseNanoToMSeconds(process.hrtime(start));
     totaltime += seconds;
     console.log(totaltime / finished);
-    await pop3.QUIT();    
     return seconds;
 }
 
@@ -57,7 +63,9 @@ async function start() {
     await addUsers(connections);
     const emails = [];
     for (let i = 0; i < connections; i += 1) {
-        emails.push(getEmail(i));
+        setTimeout(() => {
+            emails.push(getEmail(i));
+        }, i * 3);
     }
     const seconds = await Promise.all(emails);
 }
